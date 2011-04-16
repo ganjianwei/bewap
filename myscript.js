2271883fac227bf2cb453fe2c3c46cbeab8de26d
Alex Beutel <alex.beutel@gmail.com>
2011-04-16 15:52:51 -0400

    add options

--- myscript.js
+++ myscript.js
@@ -6,6 +6,7 @@
 chrome.extension.sendRequest({location: document.location}, function(response) {
 	console.log(document.location.host);
 	console.log(response.shouldBlock);
+	console.log(response.service);
 	var BEUTEL_SITE = "http://alexbeutel.com/bewap/";
     var sites = {
         "recaptcha" : BEUTEL_SITE + "recaptcha.php",
@@ -13,7 +14,10 @@ chrome.extension.sendRequest({location: document.location}, function(response) {
     };
 	// response.shouldBlock is true or false
 	if (response.shouldBlock) {
-		document.location = sites["flashcards"] + "?uid=" + response.uid + "&w=1";
+		var service = sites["flashcards"];
+		if(response.service && sites[response.service])
+			service = sites[response.service];
+		document.location = service + "?uid=" + response.uid + "&w=1";
 	} else if (response.finishedTask) {
 		console.log("Destination: %s", response.destination);
 		document.location = "http://" + response.destination;
