89391f5379ae1aacb13e34dbdd68579873a1107b
ganjianwei <jianwei.gan@gmail.com>
2011-04-15 22:30:07 -0400

    New icon. Lets you use flashcards.

--- myscript.js
+++ myscript.js
@@ -6,10 +6,14 @@
 chrome.extension.sendRequest({location: document.location}, function(response) {
 	console.log(document.location.host);
 	console.log(response.shouldBlock);
-	var BEUTEL_SITE = "http://alexbeutel.com/bewap/recaptcha.php";
+	var BEUTEL_SITE = "http://alexbeutel.com/bewap/";
+    var sites = {
+        "recaptcha" : BEUTEL_SITE + "recaptcha.php",
+        "flashcards" : BEUTEL_SITE + "flashcards.php"
+    };
 	// response.shouldBlock is true or false
 	if (response.shouldBlock) {
-		document.location = BEUTEL_SITE + "?uid=" + response.uid + "&w=1";
+		document.location = sites["flashcards"] + "?uid=" + response.uid + "&w=1";
 	} else if (response.finishedTask) {
 		console.log("Destination: %s", response.destination);
 		document.location = "http://" + response.destination;
